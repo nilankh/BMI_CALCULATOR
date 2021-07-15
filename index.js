531e61d12acda732bf56064524d88f4a11f4b817
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-15 22:03:30 +0530

    task1 done:

--- index.js
+++ index.js
@@ -7,35 +7,60 @@ app.use(express.urlencoded({ extended: true }))
 // when we call express.urlencoded() method,this method return  a function a middleware function, this middleware function parses incoming request with urlencoded payloads, that is req with body like this key=value&key=value
 var ans = []
 const data = require('./data/data.json')
-console.log("-----data Input----")
+console.log('-----data Input----')
 console.log(data)
 
 for (const i of data) {
-    var weight = i["WeightKg"]
-    var height = i["HeightCm"]/100
-    
-    // console.log(typeof(weight))
-    // console.log(typeof(height))
-    var bmi = weight / (height * height)
-    var temp = {}
-    temp["bmi"]= bmi
-    // calculate health risk
+  var weight = i['WeightKg']
+  var height = i['HeightCm'] / 100
 
-    // temp["Health"]    
-    ans.push(temp)  
-    // console.log(i["HeightCm"])
+  // console.log(typeof(weight))
+  // console.log(typeof(height))
+  var bmi = weight / (height * height)
+  console.log('BMIII', bmi)
+  var temp = []
+  temp['bmi'] = bmi
+  // calculate BMI category
+  //   Calculate Health risk
+  var g = ''
+  var h = ''
+  if (bmi < 18.5) {
+    g = 'Underweight'
+    h = 'Malnutrition risk'
+  } else if (18.5 <= bmi && bmi <= 24.9) {
+    g = 'Normal'
+    h = 'low risk'
+  } else if (25 <= bmi && bmi <= 29.9) {
+    g = 'Overweight'
+    h = 'Enhanced Risk'
+  } else if (30 <= bmi && bmi <= 34.9) {
+    g = 'Moderately obese'
+    h = 'Medium Risk'
+  } else if (35 <= bmi && bmi <= 39.9) {
+    g = 'Severely obese'
+    h = 'High risk'
+  } else {
+    g = 'Very severly obese'
+    h = 'very high risk'
+  }
+  temp['Bmi Category'] = g
+  temp['Health risk'] = h
+  ans.push(temp)
+
+  // console.log(i["HeightCm"])
 }
-console.log("Data output------")
-console.log(ans)
+
+console.log('Data output------')
+console.log('ANS', ans)
 
 app.get('/bmicalculator', (req, res) => {
   return res.sendFile(__dirname + '/bmi.html')
 })
-app.post('/bmicalculator', (req, res) =>{
-    var weight = parseFloat(req.body.weight);
-    var height = parseFloat(req.body.height);
-    var bmi = weight/(height*height)
-    res.send("BMI IS :" +bmi);
+app.post('/bmicalculator', (req, res) => {
+  var weight = parseFloat(req.body.weight)
+  var height = parseFloat(req.body.height)
+  var bmi = weight / (height * height)
+  res.send('BMI IS :' + bmi)
 })
 const port = process.env.PORT || 8000
 app.listen(8000, () => {
